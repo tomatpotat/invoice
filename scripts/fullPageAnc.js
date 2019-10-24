e78fd7dd6b959043393464bb1e67408f00a213ad
Tomat Potat <tomatopotato@tuta.io>
2019-10-24 13:10:52 +0500

    Fix broken height

--- scripts/fullPageAnc.js
+++ scripts/fullPageAnc.js
@@ -3,6 +3,7 @@ var myFullpage = new fullpage('#fullpage', {
     anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
     menu: '#menu',
     lazyLoad: true,
-    css3: false
+    css3: false,
+    scrollOverflow: true
   //  responsiveWidth: 900
     });
